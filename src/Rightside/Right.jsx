1f2634187f103bf719f9f817608da6bd72ea6b26
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-06-20 16:49:14 +0300

    finished the code

--- src/Rightside/Right.jsx
+++ src/Rightside/Right.jsx
@@ -3,6 +3,7 @@ import './right.css'
 function Right() {
   return (
     <div>
+        <h2 className='hightext'>Right Controler</h2>
         <div className="rightcontainer">
             <div className="rightclass">
                 <h2 className="righttext">
