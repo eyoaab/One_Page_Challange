c8c07f81a243f9f5e6ba80b400344c14a6e1158d
Eyob Tariku <134966084+eyoaab@users.noreply.github.com>
2024-06-20 16:38:15 +0300

    finished the right part

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './Navbar/navbar.jsx';
 import './app.css';
 import Leftside from './Leftside/Leftside.jsx';
 import Center from './Center/center.jsx';
+import Right from './Rightside/Right.jsx';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <div className="Appcontainer">
         <div className="left"><Leftside /></div>
         <div className="center"><Center /></div>
-        <div className="right">right</div>
+        <div className="right"><Right /></div>
       </div>
     </>
   );
---
+++ src/Rightside/Right.jsx
@@ -0,0 +1,60 @@
+import './right.css'
+
+function Right() {
+  return (
+    <div>
+        <div className="rightcontainer">
+            <div className="rightclass">
+                <h2 className="righttext">
+                    right Text
+                </h2>
+            </div>
+
+            <div className="rightclass">
+                <h2 className="righttext">
+                    right Text
+                </h2>
+            </div>
+            <div className="rightclass">
+                <h2 className="righttext">
+                    right Text
+                </h2>
+            </div>
+            <div className="rightclass">
+                <h2 className="righttext">
+                    right Text
+                </h2>
+            </div>
+            <div className="rightclass">
+                <h2 className="righttext">
+                    right Text
+                </h2>
+            </div>
+            <div className="rightclass">
+                <h2 className="righttext">
+                    right Text
+                </h2>
+            </div> <div className="rightclass">
+                <h2 className="righttext">
+                    right Text
+                </h2>
+            </div> <div className="rightclass">
+                <h2 className="righttext">
+                    right Text
+                </h2>
+            </div> <div className="rightclass">
+                <h2 className="righttext">
+                    right Text
+                </h2>
+            </div> <div className="rightclass">
+                <h2 className="righttext">
+                    right Text
+                </h2>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Right
